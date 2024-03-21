c1ab9588340d73cfd94cadea82cabaf90cbd2ac5
oruccakir <oruccakir2525@gmail.com>
2024-03-21 09:51:45 +0300

    homework is not understood well

--- examsim.py
+++ examsim.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import comb
 
-
 # Binomial PMF calculation
 def binomial_pmf(k, n, p):
     return comb(n, k) * (p**k) * ((1-p)**(n-k))
@@ -51,37 +50,68 @@ def question_a():
     plt.ylabel('Probability')
     plt.savefig('pzpmf.png')
 
+def question_b():
+    # Initial probabilities at the start of the exam
+    t = 200  # Full time available
+    p_c = 0.6 + (2 * t - 40) / 1000  # Probability of a correct answer
+    p_w = 0.2 + (30 - t) / 1000      # Probability of a wrong answer
+    p_b = 1 - (p_c + p_w)            # Probability of leaving a question blank
+
+    n = 100  # Total number of questions
+    M = int(input("Enter the score M: "))  # Required score
+
+    # Probability of scoring more than M points
+    total_probability = 0
+    for correct in range(0, n + 1):
+        for wrong in range(0, n - correct + 1):
+            net_points = 5*correct - 1.25*wrong 
+            if net_points >= M:
+                prob_correct = binomial_pmf(correct, n, p_c)
+                prob_wrong = binomial_pmf(wrong, n - correct, p_w)
+                blank = n - correct - wrong
+                prob_blank = binomial_pmf(blank, n - correct - wrong, p_b)
+                total_probability += prob_correct * prob_wrong * prob_blank
+
+    print(f"Probability of scoring more than {M} points: {total_probability}")
 
 def question_c():
     # Probabilities with 120 minutes remaining
     t = 120
-    p_c = 0.6 + (2*t - 40) / 1000  # probability of a correct answer
-    p_w = 0.2 + (30 - t) / 1000    # probability of a wrong answer
+    p_c = 0.6 + (2*t - 40) / 1000  # Probability of a correct answer
+    p_w = 0.2 + (30 - t) / 1000    # Probability of a wrong answer
 
-    # Current state
-    correct_so_far = 20
-    wrong_so_far = 7
-    blank_so_far = 4
-    remaining_questions = 100 - (correct_so_far + wrong_so_far + blank_so_far)
+    # Initial condition
+    initial_correct = 20
+    initial_wrong = 7
+    initial_blank = 4
+    total_initial_questions = initial_correct + initial_wrong + initial_blank
 
-    # Current net points
-    current_net_points = correct_so_far - wrong_so_far / 4
-    current_score = current_net_points * 5
+    # Probability of the initial condition
+    prob_initial_condition = (comb(total_initial_questions, initial_correct) * p_c**initial_correct *
+                            comb(total_initial_questions - initial_correct, initial_wrong) * p_w**initial_wrong)
 
-    # Required additional net points to pass
-    required_additional_net = (200 - current_score) / 5
+    # Remaining questions
+    remaining_questions = 100 - total_initial_questions
 
-    # Calculate the probability of passing
-    probability_pass = 0
+    # Current score
+    current_score = initial_correct * 5 - (initial_wrong // 4) * 5
+
+    # Required additional score to pass
+    required_additional_score = 200 - current_score
+
+    # Calculate the probability of passing given the initial condition
+    probability_pass_given_condition = 0
     for correct in range(0, remaining_questions + 1):
         for wrong in range(0, remaining_questions - correct + 1):
-            net_points = correct - wrong / 4
-            if net_points >= required_additional_net:
+            net_points = correct - wrong // 4
+            additional_score = net_points * 5
+            if additional_score >= required_additional_score:
                 prob_correct = comb(remaining_questions, correct) * (p_c ** correct) * ((1 - p_c) ** (remaining_questions - correct))
                 prob_wrong = comb(remaining_questions - correct, wrong) * (p_w ** wrong) * ((1 - p_w) ** (remaining_questions - correct - wrong))
-                probability_pass += prob_correct * prob_wrong
+                probability_pass_given_condition += (prob_correct * prob_wrong) / prob_initial_condition
 
-    print("Probability of passing the exam (s):", probability_pass)
+    print("Conditional probability of passing the exam (s):", probability_pass_given_condition)
 
 
-question_a()
+
+question_b()
