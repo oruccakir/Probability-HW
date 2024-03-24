2c54bce8fc062eb4fe4ee7f543e0872b26b783d2
oruccakir <oruccakir2525@gmail.com>
2024-03-24 22:11:02 +0300

    homework is completed

--- examsim.py
+++ examsim.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import comb
+from scipy.stats import binom
 from scipy.integrate import quad
 
-from scipy.stats import erlang
 
 # Binomial PMF calculation
 def binomial_pmf(k, n, p):
@@ -19,9 +19,6 @@ def question_a():
     p_w = 0.2 + (30 - t) / 1000    # probability of a wrong answer
     p_b = 1 - (p_c + p_w)          # probability of leaving blank
 
-    print("Probability of a correct answer (p_c):", p_c)
-    print("Probability of a wrong answer (p_w):", p_w)
-    print("Probability of leaving blank (p_b):", p_b)
     n = 100  # total number of questions
 
     # Arrays to store PMF valu           es
@@ -64,7 +61,7 @@ def question_b():
     p_b = 1 - (p_c + p_w)            # Probability of leaving a question blank
 
     n = 100  # Total number of questions
-    M = int(input("Enter the score M: "))  # Required score
+    M = int(input())  # Required score
 
     # Probability of scoring more than M points
     total_probability = 0
@@ -74,7 +71,8 @@ def question_b():
             if net_points > M:
                 total_probability += calculate_probabilities(correct, wrong, n - correct - wrong, n, p_c, p_w, p_b)
 
-    print(f"Probability of scoring more than {M} points: {total_probability}")
+    print(f"mtMor400: {total_probability}")
+    return M
 
 def question_c():
     # Probabilities with 120 minutes remaining
@@ -109,10 +107,9 @@ def question_c():
             if additional_score >= required_additional_score:
                 probability_pass_given_condition += calculate_probabilities(correct,wrong,blank,remaining_questions,p_c,p_w,p_b)
 
-    print("Conditional probability of passing the exam (s):", probability_pass_given_condition)
+    print("s:", probability_pass_given_condition)
 
-def question_d():
-    M = int(input("Enter the score M: "))  # Required score
+def question_d(M):
     n = 100  # Total number of questions
      # Initial probabilities at the start of the exam
     t = 200  # Full time available
@@ -127,71 +124,62 @@ def question_d():
             blank = n - correct - wrong
             if blank >= 8 and net_points > M:  # Skip if less than 8 questions are left blank
                 probability_pass_given_condition += calculate_probabilities(correct,wrong,blank,n,p_c,p_w,p_b)
+            
+    divider = 0
+    for i in range(0, 8):
+        divider += comb(n,i)*(p_b**i)*((1-p_b)**(n-i))
+    divider = 1 - divider
+    print("news:", probability_pass_given_condition/divider)
 
-    print("Conditional probability of passing the exam (s):", probability_pass_given_condition)
-    
+
+def phase_probability(questions, t, correct, incorrect,blank):
+    pc = 0.6 + (2 * t - 40) / 1000
+    pw = 0.2 + (30 - t) / 1000
+    pb = 1 - pc - pw
+
+    # Binomial probabilities for correct and incorrect answers
+    prob_correct = binom.pmf(correct, questions, pc)
+    prob_incorrect = binom.pmf(incorrect, questions - correct, pw)
+    prob_blank = binom.pmf(blank, questions - correct - incorrect, pb)
+
+    # Return combined probability
+    return prob_correct * prob_incorrect*prob_blank
+
+
+def compute_score(correct, incorrect):
+    net_correct = correct - incorrect // 4
+    return max(net_correct, 0) * 5
 
 def question_e():
-    n = 100  # Total number of questions
-    t = 200  # Full time available
-    points_needed = 300
-    breaks = [4, 6, 0]  # Break times after each phase
-    phases = [(20, 40), (30, 40), (50, 110)]  # (questions, time) for each phase
-    probability_less_than_300 = 0
-    total_points = 0    # Total points obtained so far
-    map_20 = {}
-    map_30 = {}
-    map_50 = {}
+    total_prob = 0
+    probs1 = []
+    probs2 = []
+    probs3 = []
 
-    for i, phase in enumerate(phases):
-        questions, time = phase
-        print("Questions:", questions, "Time:", time)
-        print("Remaining time:", t)
-        p_c = 0.6 + (2 * t - 40) / 1000  # Probability of a correct answer
-        p_w = 0.2 + (30 - t) / 1000      # Probability of a wrong answer
-        p_b = 1 - (p_c + p_w)            # Probability of leaving a question blank
-
-        for correct in range(0, questions + 1):
-            for wrong in range(0, questions - correct + 1):
-                net_points = correct - wrong // 4
-                if net_points < 0:
-                    net_points = 0
-                net_points = net_points * 5
-                if net_points < points_needed: 
-                    blank = questions - correct - wrong
-                    if questions == 20:
-                        map_20[(correct, wrong, blank)] = 5*(correct - wrong//4)
-                    elif questions == 30:
-                        map_30[(correct, wrong, blank)] = 5*(correct - wrong//4)
-                    else:
-                        map_50[(correct, wrong, blank)] = 5*(correct - wrong//4)
-                    #probability_less_than_300 += calculate_probabilities(correct, wrong, blank, questions, p_c, p_w, p_b)
-        
+    for correct1 in range(21):
+        for incorrect1 in range(21 - correct1):
+            blank1 = 20 - correct1 - incorrect1
+            probs1.append([phase_probability(20, 200, correct1, incorrect1, blank1),compute_score(correct1, incorrect1)])
     
-    t20 = 200
-    t30 = 156
-    t50 = 110
+    for correct2 in range(31):
+        for incorrect2 in range(31 - correct2):
+            blank2 = 30 - correct2 - incorrect2
+            probs2.append([phase_probability(30, 156, correct2, incorrect2, blank2),compute_score(correct2, incorrect2)])
+    
+    for correct3 in range(51):
+        for incorrect3 in range(51 - correct3):
+            blank3 = 50 - correct3 - incorrect3
+            probs3.append([phase_probability(50, 110, correct3, incorrect3, blank3),compute_score(correct3, incorrect3)])
+    # Iterate through all combinations for each phase
+    for prob1,score1 in probs1:
+        for prob2, score2 in probs2:
+            for prob3, score3 in probs3:
+                score = 0
+                score += score1 + score2 + score3
+                if score < 300:
+                    total_prob += prob1 * prob2 * prob3
 
-    for key20, value20 in map_20.items():
-        p_c20 = 0.6 + (2 * t20 - 40) / 1000  
-        p_w20 = 0.2 + (30 - t20) / 1000      
-        p_b20 = 1 - (p_c20 + p_w20)            
-        for key30, value30 in map_30.items():
-            p_c30 = 0.6 + (2 * t30 - 40) / 1000
-            p_w30 = 0.2 + (30 - t30) / 1000
-            p_b30 = 1 - (p_c30 + p_w30)
-            for key50, value50 in map_50.items():
-                p_c50 = 0.6 + (2 * t50 - 40) / 1000
-                p_w50 = 0.2 + (30 - t50) / 1000
-                p_b50 = 1 - (p_c50 + p_w50)
-                total_points = value20 + value30 + value50
-                if total_points < points_needed:
-                    probability_less_than_300 += calculate_probabilities(key20[0],key20[1],key20[2],20,p_c20, p_w20, p_b20)
-                    probability_less_than_300 += calculate_probabilities(key30[0],key30[1],key30[2],30,p_c30, p_w30, p_b30)
-                    probability_less_than_300 += calculate_probabilities(key50[0],key50[1],key50[2],50,p_c50, p_w50, p_b50)
-
-    print("Probability of getting less than 300 points:", probability_less_than_300)
-
+    print("It300:", total_prob)
 
 def question_f():
     lambda_1 = 1 / 40  # Rate parameter for phase 1
@@ -199,7 +187,7 @@ def question_f():
 
     # Calculate the probability that phase 1 lasts longer than 25 minutes
     prob = np.exp(-lambda_1 * x)
-    print("Probability that phase 1 lasts longer than 25 minutes:", prob)
+    print("p1m25:", prob)
 
     # Simulate this process 1000 times
     simulation_times = np.random.exponential(scale=1/lambda_1, size=1000)
@@ -211,8 +199,6 @@ def question_f():
 
 
     # Calculate the empirical probability
-    empirical_prob = np.mean(exceeds_25_minutes)
-    print(f"The empirical probability from the simulation is: {empirical_prob}")
     plt.plot(counts, probs, label='Count Probability Function')
     plt.xlabel('Number of Simulations')
     plt.xlim(0, 1000)
@@ -233,7 +219,7 @@ def question_g():
     lambda_exp = 1/40.0  # The rate parameter of the exponential distribution
     # Calculate the probability between 60 and 80 minutes using the custom Erlang PDF
     prob_60_80, _ = quad(lambda x: erlang_pdf(n, lambda_exp, x), 60, 80)
-    print(f"The probability that the solution time is between 60 and 80 minutes is: {prob_60_80}")
+    print(f"b6080: {prob_60_80}")
 
     # Plot the Erlang PDF
     x_values = np.linspace(0, 160, 1000)  # Values for x-axis
@@ -243,17 +229,20 @@ def question_g():
     plt.fill_between(x_values, y_values, where=((x_values >= 60) & (x_values <= 80)), color='red', alpha=0.3, label='Area between 60 and 80')
     plt.xlabel('Time (minutes)')
     plt.ylabel('Probability Density')
+    plt.ylim(0, 0.01)
     plt.title('Erlang PDF for the Solution Time of 30 Questions')
     plt.legend()
     plt.grid(True)
     plt.savefig('erlangpdf.png')
     plt.close()
 
-    # Compare with the built-in Erlang distribution from scipy
-    erlang_dist = erlang(n, scale=1/lambda_exp)
-    prob_60_80_builtin = erlang_dist.cdf(80) - erlang_dist.cdf(60)
-    print(f"Built-in Erlang probability: {prob_60_80_builtin}")
 
 
-question_b()
-question_d()
+
+question_a()
+M = question_b()
+question_c()
+question_d(M)
+question_e()
+question_f()
+question_g()
